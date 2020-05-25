5583306e7e245deee9a8f7605cbb91b740bd4dec
MicheleMozzanica <mozzo2000@gmail.com>
2020-05-25 16:30:18 +0200

    aggiunta main e creazione classe equilibrio

--- src/tamaGolem/Main.java
+++ src/tamaGolem/Main.java
@@ -1,9 +1,50 @@
 package tamaGolem;
 
+import mylib.*;
+
 public class Main {
+
+	private final static String MESS_BENVENUTO = "BENVENUTO IN TAMAGOLEM!";
+	private final static String INSERT_NOME_GIOCATORE = "Inserisci il tuo nome: ";
+	private final static String CHOOSE_OPTIONS = "Scegli cosa fare: ";
+	private static final String MESS_ARRIVEDERCI = "Nel nome del padre, del figlio e dello spirito santo\nAndate in pace!";
+	private static final String MESS_ERROR = "ATTENZIONE, SEI STRONZO!";
+
+	private final static String[] MAIN_MENU_ITEMS = { "Vai allo scontro"};
+
+	private final static int NUM_PIETRE = 5;
+	private final static int NUM_TAMAGOLEM = 3;
+
+	//private static TamaGolem Golem = new TamaGolem();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println(MESS_BENVENUTO + "\n\n");
+
+		String inserisciNome = InputDati.leggiStringa(INSERT_NOME_GIOCATORE);
+
+		MyMenu principale = new MyMenu(CHOOSE_OPTIONS, MAIN_MENU_ITEMS);
+
+		boolean fine = false;
+
+		do {
+			int voceSelezionata = principale.scegli();
+
+			switch (voceSelezionata) {
+			case 1:
+				// vai allo scontro
+				break;
+			case 0:
+				fine = true;
+				System.out.println(MESS_ARRIVEDERCI);
+				break;
+			default:
+				System.out.println(MESS_ERROR);
+				break;
+			}// switch
+
+		} while (!fine);
 
 	}
 
